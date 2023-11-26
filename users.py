147207596b4b7d1d74e9a81433a95d7b4d202563
unknown <danessti@gmail.com>
2023-11-25 22:47:23 -0500

    peticiones http post, put y delete en py

--- users.py
+++ users.py
@@ -1,3 +1,5 @@
+from operator import index
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -44,6 +46,46 @@ async def user(id: int):
 @app.get("/user/")
 async def user(id: int):
     return search_user(id)
+
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:
+        users_list.append(user)
+        return user
+
+
+@app.put("/user/")
+async def user(user: User):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if not found:
+        return {"error": "No se ha actualizado el usuario"}
+    else:
+        return user
+
+
+@app.delete("/user/{id}")
+async def user(id: int):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+
+    if not found:
+        return {"error": "No se ha eliminado el usuario"}
+
 
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
